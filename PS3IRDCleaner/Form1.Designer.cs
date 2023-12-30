6d2153959cea59d56892a8a67d9446998d015fe5
FlexBy420 <flexby420@gmail.com>
2023-12-31 00:20:15 +0100

    added source code

---
+++ PS3IRDCleaner/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace IsoTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bStart.Location = new System.Drawing.Point(258, 150);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // tbResults
+            // 
+            this.tbResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResults.Location = new System.Drawing.Point(12, 25);
+            this.tbResults.Multiline = true;
+            this.tbResults.Name = "tbResults";
+            this.tbResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbResults.Size = new System.Drawing.Size(321, 119);
+            this.tbResults.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 185);
+            this.Controls.Add(this.tbResults);
+            this.Controls.Add(this.bStart);
+            this.Name = "Form1";
+            this.Text = "PS3IRDToMD5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.TextBox tbResults;
+    }
+}
+
---
+++ PS3IRDCleaner/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using IsoTools.Iso9660;
+
+namespace IsoTools {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            string[] cmdLine = Environment.GetCommandLineArgs();
+            if (cmdLine.Length == 2 && File.Exists(cmdLine[1]) && !File.Exists(Path.ChangeExtension(cmdLine[1], ".md5"))) {
+                List<PS3File> lvFilesRaw = new List<PS3File>();
+                IrdFile irdFile = IrdFile.Load(cmdLine[1]);
+                PS3CDReader pS3CDReader = new PS3CDReader(irdFile.Header);
+                ICollection<DirectoryMemberInformation> members = pS3CDReader.Members;
+                foreach (DirectoryMemberInformation directoryMemberInformation in (from d in members
+                                                                                   where d.IsFile
+                                                                                   select d).Distinct<DirectoryMemberInformation>())
+                    lvFilesRaw.Add(new PS3File() { Lenght = directoryMemberInformation.Length, Filename = directoryMemberInformation.Path, MD5 = irdFile.FileHashes.FirstOrDefault<KeyValuePair<long, byte[]>>((KeyValuePair<long, byte[]> f) => f.Key == directoryMemberInformation.StartSector).Value.AsString() });
+
+                using (TextWriter tw = File.CreateText(Path.ChangeExtension(cmdLine[1], ".md5")))
+                    foreach (PS3File F in lvFilesRaw)
+                        tw.WriteLine("{0} *{1}", F.MD5, F.Filename.TrimStart('/'));
+
+                Application.Exit();
+            }
+        }
+
+        struct PS3File {
+            public string Filename;
+            public long Lenght;
+            public string MD5;
+        }
+
+        private void bStart_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog() { Title = "Select an IRD File", Filter = "IRD files (*.ird)|*.ird" };
+            if (ofd.ShowDialog() != System.Windows.Forms.DialogResult.OK || Path.GetExtension(ofd.FileName) != ".ird") {
+                MessageBox.Show("Not and IRD File?");
+                return;
+            }
+            SaveFileDialog sfd = new SaveFileDialog() { Title = "Select an MD5 File", Filter = "MD5 files (*.md5)|*.md5" };
+            if (sfd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            this.Enabled = false;
+
+            List<PS3File> lvFilesRaw = new List<PS3File>();
+            IrdFile irdFile = IrdFile.Load(ofd.FileName);
+            PS3CDReader pS3CDReader = new PS3CDReader(irdFile.Header);
+            ICollection<DirectoryMemberInformation> members = pS3CDReader.Members;
+            foreach (DirectoryMemberInformation directoryMemberInformation in (from d in members
+                                                                               where d.IsFile
+                                                                               select d).Distinct<DirectoryMemberInformation>())
+                lvFilesRaw.Add(new PS3File() { Lenght = directoryMemberInformation.Length, Filename = directoryMemberInformation.Path, MD5 = irdFile.FileHashes.FirstOrDefault<KeyValuePair<long, byte[]>>((KeyValuePair<long, byte[]> f) => f.Key == directoryMemberInformation.StartSector).Value.AsString() });
+
+            using (TextWriter tw = File.CreateText(sfd.FileName))
+                foreach (PS3File F in lvFilesRaw)
+                    tw.WriteLine("{0} *{1}", F.MD5, F.Filename.TrimStart('/'));
+
+            tbResults.Text = "MD5 Created Successfully";
+            this.Enabled = true;
+        }
+    }
+}
